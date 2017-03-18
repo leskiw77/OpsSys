bc52fce3498f6ca9dd915ade3232c4fc42b14703
Kamil Borowiec <kborowiec95@gmail.com>
2017-03-18 14:44:30 +0100

    zad3 prawie dzialajace

--- cw02/zad3/main.c
+++ cw02/zad3/main.c
@@ -10,261 +10,221 @@
 
 void printMenu(void) {
     printf("\n%s", "MENU : \n");
-    printf("1) Ustawienie rygla na wybrany znak pliku\n");
-    printf("2) Ustawienie rygla na wybrany znak pliku\n");
+    printf("1) Ustawienie rygla na odczyt wybranego znaku pliku\n");
+    printf("2) Ustawienie rygla na zapis wybranego znaku pliku\n");
     printf("3) Wyświetlenie listy zaryglowanych znaków pliku,\n");
-    // ( +nr PID procesu będącego właścicielem rygla oraz jego typie - odczyt/zapis)
     printf("4) Zwolnienie wybranego rygla\n");
     printf("5) Odczyt wybranego znaku pliku\n");
     printf("6) Zmiana wybranego znaku pliku\n");
     printf("0) Wyjscie\n : ");
 }
 
+bool setLock(int fd, int byteNumber, char type) {
+    if (type != 'w' && type != 'r') {
+        return false;
+    }
+    struct flock lck;
+    if (type == 'w') {
+        lck.l_type = F_WRLCK;
+    } else {
+        lck.l_type = F_RDLCK;
+    };
+    lck.l_start = byteNumber;
+    lck.l_whence = SEEK_SET;
+    lck.l_len = 1;
+    if (fcntl(fd, F_SETLK, &lck) == -1) {
+        if (errno != EACCES && errno != EAGAIN) {
+            printf("Blad 'fcntl' : %s errno: %d", strerror(errno), errno);
+            return false;
+        }
+    }
+    return true;
+}
 
-void printFileLocks(int fd){
+bool resetLock(int fd, int byteNumber) {
+    struct flock lck;
+    lck.l_type = F_UNLCK;
+    lck.l_start = byteNumber;
+    lck.l_whence = SEEK_SET;
+    lck.l_len = 1;
+    if (fcntl(fd, F_SETLK, &lck) == -1) {
+        if (errno != EACCES && errno != EAGAIN) {
+            printf("Blad 'fcntl' : %s \nerrno: %d", strerror(errno), errno);
+            return false;
+        }
+    }
+    return true;
+}
+
+bool printLocks(int fd) {
     int i;
-    struct flock lock;
+    struct flock lck;
     int size;
 
     size = lseek(fd, 0, SEEK_END);
     lseek(fd, 0, SEEK_SET);
 
-    printf("Blokady : \n");
-    for(i=0 ; i<size ; ++i){
-        lock.l_len = 1;
-        lock.l_start = i;
-        lock.l_type = F_RDLCK;
-        lock.l_whence = SEEK_SET;
+    for (i = 0; i < size; i++) {
+        // blokady odczytu :
+        lck.l_start = i;
+        lck.l_len = 1;
+        lck.l_type = F_RDLCK;
+        lck.l_whence = SEEK_SET;
 
-        if(fcntl(fd, F_GETLK, &lock) == -1){
-            printf("Blad w funkcji listOfLocks: \"%s\"\terrno: %d\n",strerror(errno),errno);
-            return -1;
+        if (fcntl(fd, F_GETLK, &lck) == -1) {
+            printf("Blad 'fcntl' : %s \nerrno: %d", strerror(errno), errno);
+            return false;
         }
 
-
-        if(lock.l_type != F_UNLCK){
-            printf("PID %d: ma blokade odczytu na bajcie %d\n", (int)lock.l_pid, (int)lock.l_start);
-        }
-        //test rygla zapisu
-        lock.l_len = 1;
-        lock.l_start = i;
-        lock.l_type = F_WRLCK;
-        lock.l_whence = SEEK_SET;
-
-        if(fcntl(fd, F_GETLK, &lock) == -1){
-            printf("Blad w funkcji listOfLocks: \"%s\"\terrno: %d\n",strerror(errno),errno);
-            return -1;
+        if (lck.l_type != F_UNLCK) {
+            printf("Blokada odczytu : Bajt nr : %d, PID %d \n", (int) lck.l_start, (int) lck.l_pid);
         }
 
-        if(lock.l_type != F_UNLCK){
-            printf("PID %d: ma blokade zapisu na bajcie %d\n", (int)lock.l_pid, (int)lock.l_start);
+        // blokady zapisu :
+        lck.l_start = i;
+        lck.l_len = 1;
+        lck.l_type = F_WRLCK;
+        lck.l_whence = SEEK_SET;
+
+        if (fcntl(fd, F_GETLK, &lck) == -1) {
+            printf("Blad 'fcntl' : %s \nerrno: %d", strerror(errno), errno);
+            return false;
+        }
+
+        if (lck.l_type != F_UNLCK) {
+            printf("Blokada zapisu : Bajt nr : %d, PID %d \n", (int) lck.l_start, (int) lck.l_pid);
         }
     }
-    return 0;
+    return true;
 }
 
-bool writeByte(int fd, int byteNumber, char byteValue){
+
+bool writeByte(int fd, int byteNumber, char byteValue) {
     struct flock lck;
     lck.l_len = 1;
     lck.l_start = byteNumber;
     lck.l_type = F_WRLCK;
     lck.l_whence = SEEK_SET;
 
-    if(fcntl(fd, F_GETLK, &lck) == -1){
-        printf("Blad w funkcji writeChar: \"%s\"\terrno: %d\n",strerror(errno),errno);
+    if (fcntl(fd, F_GETLK, &lck) == -1) {
+        printf("Blad 'fcntl' : %s \nerrno: %d", strerror(errno), errno);
         return false;
     }
-
-    if(lck.l_type != F_UNLCK){
-        printf("Na ten znak zalozona jest blokada i nie da sie zapisywac\n");
+    if (lck.l_type != F_UNLCK) {
+        printf("Blokada uniemozliwia zapis");
         return false;
     }
-
-    if(lseek(fd, byteNumber, SEEK_SET) == -1){
-        printf("Blad w funkcji writeChar: \"%s\"\terrno: %d\n",strerror(errno),errno);
+    if (lseek(fd, byteNumber, SEEK_SET) == -1) {
+        printf("Blad 'lseek' : %s \nerrno: %d", strerror(errno), errno);
         return false;
     }
-
-    char * buff = (char*)malloc(sizeof(char));
+    char *buff = (char *) malloc(sizeof(char));
     buff[0] = byteValue;
-    if(write(fd,buff, 1) == -1){
-        printf("Blad w funkcji readChar: \"%s\"\terrno: %d\n",strerror(errno),errno);
+    if (write(fd, buff, 1) == -1) {
+        printf("Blad 'write' : %s \nerrno: %d", strerror(errno), errno);
         return false;
     }
     free(buff);
     return true;
 }
 
-char readChar(int fileDesc, int offset){
-    struct flock lock;
-    char ch[2];
+bool readByte(int fd, int byteNumber, char *value) {
+    struct flock lck;
+    lck.l_len = 1;
+    lck.l_start = byteNumber;
+    lck.l_type = F_RDLCK;
+    lck.l_whence = SEEK_SET;
 
-    lock.l_len = 1;
-    lock.l_start = offset;
-    lock.l_type = F_RDLCK;
-    lock.l_whence = SEEK_SET;
-
-    if(fcntl(fileDesc, F_GETLK, &lock) == -1){
-        printf("Blad w funkcji readChar: \"%s\"\terrno: %d\n",strerror(errno),errno);
-        return -1;
+    if (fcntl(fd, F_GETLK, &lck) == -1) {
+        printf("Blad 'fcntl' : %s \nerrno: %d", strerror(errno), errno);
+        return false;
     }
 
-    if(lock.l_type == F_WRLCK){
-        printf("Na ten znak zalozona jest blokada i nie da sie go odczytac\n");
-        return -1;
+    if (lck.l_type == F_WRLCK) {
+        printf("Blokada uniemozliwia odczyt");
+        return false;
     }
 
-    if(lseek(fileDesc, offset, SEEK_SET) == -1){
-        printf("Blad w funkcji readChar: \"%s\"\terrno: %d\n",strerror(errno),errno);
-        return -1;
+    if (lseek(fd, byteNumber, SEEK_SET) == -1) {
+        printf("Blad 'lseek' : %s \nerrno: %d", strerror(errno), errno);
+        return false;
     }
 
-    if((read(fileDesc, &ch, 1)) == -1){
-        printf("Blad w funkcji readChar: \"%s\"\terrno: %d\n",strerror(errno),errno);
-        return -1;
+    char buff[2];
+    if ((read(fd, &buff, 1)) == -1) {
+        printf("Blad 'read' : %s \nerrno: %d", strerror(errno), errno);
+        return false;
     }
-    //printf("%c\n", znak);
-    return ch[0];
+    *value = buff[0];
+    return true;
 }
 
-
-int listOfLocks(int fileDesc){
-    int i;
-    struct flock lock;
-    int size;
-
-    size = lseek(fileDesc, 0, SEEK_END);
-    lseek(fileDesc, 0, SEEK_SET);
-
-    for(i=0 ; i<size ; ++i){
-        //test rygla odczytu
-        lock.l_len = 1;
-        lock.l_start = i;
-        lock.l_type = F_RDLCK;
-        lock.l_whence = SEEK_SET;
-
-        if(fcntl(fileDesc, F_GETLK, &lock) == -1){
-            printf("Blad w funkcji listOfLocks: \"%s\"\terrno: %d\n",strerror(errno),errno);
-            return -1;
-        }
-
-        if(lock.l_type != F_UNLCK){
-            printf("PID %d: ma blokade odczytu na bajcie %d\n", (int)lock.l_pid, (int)lock.l_start);
-        }
-        //test rygla zapisu
-        lock.l_len = 1;
-        lock.l_start = i;
-        lock.l_type = F_WRLCK;
-        lock.l_whence = SEEK_SET;
-
-        if(fcntl(fileDesc, F_GETLK, &lock) == -1){
-            printf("Blad w funkcji listOfLocks: \"%s\"\terrno: %d\n",strerror(errno),errno);
-            return -1;
-        }
-
-        if(lock.l_type != F_UNLCK){
-            printf("PID %d: ma blokade zapisu na bajcie %d\n", (int)lock.l_pid, (int)lock.l_start);
-        }
-    }
-
-    return 0;
-}
-
-int unlock(int fileDesc, int offset){
-    if((lock_reg(fileDesc, F_SETLK, F_UNLCK, offset, SEEK_SET, 1)) == -1){
-        if(errno == EACCES || errno == EAGAIN){
-            printf("System zdjal juz blokade z tego bajtu\n");
-        }else{
-            printf("Blad w funkcji unlock: \"%s\"\terrno: %d\n", strerror(errno), errno);
-            return -1;
-        }
-    }
-    return 0;
-}
-
-
-int writeLock(int fileDesc, int offset){
-    if((lock_reg(fileDesc, F_SETLK, F_RDLCK, offset, SEEK_SET, 1)) == -1){
-        if(errno == EACCES || errno == EAGAIN){
-            printf("System przekazal juz blokade odczytu na dany znak pliku\n");
-        }else{
-            printf("Blad w funkcji writeLock: \"%s\"\terrno: %d\n", strerror(errno), errno);
-            return -1;
-        }
-    }
-    return 0;
-}
-
-int readLock(int fileDesc, int offset){
-    if((lock_reg(fileDesc, F_SETLK, F_WRLCK, offset, SEEK_SET, 1)) == -1){
-        if(errno == EACCES || errno == EAGAIN){
-            printf("System przekazal juz blokade zapisu na dany znak pliku\n");
-        }else{
-            printf("Blad w funkcji readLock: \"%s\"\terrno: %d\n", strerror(errno), errno);
-            return -1;
-        }
-    }
-    return 0;
-}
-
-int lock_reg(int fileDesc, int cmd, int type, off_t offset, int whence, off_t len){
-    struct flock lock;
-
-    lock.l_type = type;
-    lock.l_start = offset;
-    lock.l_whence = whence;
-    lock.l_len = len;
-
-    return fcntl(fileDesc, cmd, &lock);
-}
-
-
-int main(int argc, char * argv[]) {
-    if(argc!=2) {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         printf("Bledna liczba argumentow");
         return 1;
     }
     int file;
-    if((file = open(argv[1], O_RDWR)) == -1){
+    if ((file = open(argv[1], O_RDWR)) == -1) {
         printf("Blad odczytu pliku");
         return 1;
     }
-    while(1) {
-        printMenu();
+    printMenu();
+    while (1) {
         char command = getchar();
-        switch (command){
+        switch (command) {
             case '0': {
                 return 0;
             }
             case '1': {
                 printf("Podaj nr bajtu : ");
-                //int byteNumber = 0;
-                //scanf("%i",&byteNumber);
-                //setLock(file,byteNumber);
+                int byteNumber = 0;
+                scanf("%i", &byteNumber);
+                if (setLock(file, byteNumber, 'r')) {
+                    printf("Blokada odczytu ustawiona\n");
+                }
             }
                 break;
             case '2': {
-
+                printf("Podaj nr bajtu : ");
+                int byteNumber = 0;
+                scanf("%i", &byteNumber);
+                if (setLock(file, byteNumber, 'w')) {
+                    printf("Blokada zapisu ustawiona\n");
+                }
             }
                 break;
             case '3': {
-                printFileLocks(file);
+                if (printLocks(file)) {
+                    printf("OK\n");
+                }
             }
                 break;
             case '4': {
-
+                printf("Podaj nr bajtu : ");
+                int byteNumber = 0;
+                scanf("%i", &byteNumber);
+                if (resetLock(file, byteNumber)) {
+                    printf("Blokada zdjeta\n");
+                }
             }
                 break;
-            case '5':{
-
+            case '5': {
+                printf("Podaj nr bajtu : ");
+                int byteNumber = 0;
+                char byteValue;
+                scanf("%i", &byteNumber);
+                if (readByte(file, byteNumber, &byteValue)) {
+                    printf("Odczytana wartosc : %c\n", byteValue);
+                }
             }
                 break;
-            case '6':{
-                printf("Podaj nr bajtu i wartosc: ");
+            case '6': {
+                printf("Podaj nr bajtu i wartosc : ");
                 int byteNumber = 0;
                 char byteValue = '0';
-                scanf("%i %c",&byteNumber,&byteValue);
-                if(writeByte(file,byteNumber,byteValue)) {
+                scanf("%i %c", &byteNumber, &byteValue);
+                if (writeByte(file, byteNumber, byteValue)) {
                     printf("OK\n");
                 }
             }
