301cff6f6114c2f29272d1e2c1dba0fe9f575dd4
Kamil Borowiec <kborowiec95@gmail.com>
2017-03-20 13:04:17 +0100

    zad2 - wersja ze stat dziala

--- cw02/zad2/main.c
+++ cw02/zad2/main.c
@@ -7,14 +7,12 @@ int main (int argc, char ** argv) {
         return 1;
     }
 
-    // otwieramy katalog :
-    DIR * directory = NULL;
-    char * fullPath = argv[1];
-    directory = opendir(fullPath);
-    if(directory==NULL) {
-        printf("Blad otwierania katalogu");
-        return 1;
+    char fullPath[256];
+    strcpy(fullPath, argv[1]);
+    if(fullPath[strlen(fullPath)-1] != '/') {
+        strcat(fullPath, "/");
     }
+
     int size = -1;
     size = atoi(argv[2]);
     if(size == -1) {
@@ -23,13 +21,12 @@ int main (int argc, char ** argv) {
     }
 
     if(strcmp(argv[3],"stat")==0){
-        printGreaterStat(directory,fullPath,size);
+        printGreaterStat(fullPath,size);
     } else if(strcmp(argv[3],"nftw")==0){
         printGreaterNftw(fullPath,100);
     } else {
         printf("Bledny tryb uruchomienia programu");
         return 1;
     }
-    closedir (directory);
     return 0;
 }
--- cw02/zad2/printerStat.c
+++ cw02/zad2/printerStat.c
@@ -1,23 +1,28 @@
 #include "printerStat.h"
 
-void printGreaterStat(DIR * dir, char * dirPath, int minSize) {
-    struct dirent * file;
+void printGreaterStat(char *dirPath, int minSize) {
+    DIR *dir = opendir(dirPath);
+    if (dir == NULL) {
+        printf("Blad otwierania katalogu %s\n", dirPath);
+        return;
+    }
+    struct dirent *file;
     struct stat st;
     while ((file = readdir(dir)) != NULL) {
         // tylko pliki regularne :
         if (file->d_type == 8) {
             // pelna nazwa pliku :
             char name[256];
-            strcpy(name,dirPath);
-            strcat(name,file->d_name);
+            strcpy(name, dirPath);
+            strcat(name, file->d_name);
 
             // pobierz informacje o pliku :
-            stat(name,&st);
+            stat(name, &st);
 
             // rozmiar :
             long size = st.st_size;
 
-            if( size >= minSize ) {
+            if (size >= minSize) {
                 // czas ostatniej modyfikacji :
                 char time[20];
                 time_t modTime = st.st_mtime;
@@ -25,21 +30,29 @@ void printGreaterStat(DIR * dir, char * dirPath, int minSize) {
 
                 // prawa dostepu :
                 char accMode[10];
-                for(int i=0;i<9;i++)  accMode[i] = '-';
-                if(st.st_mode & S_IRUSR) accMode[0] = 'r';
-                if(st.st_mode & S_IWUSR) accMode[1] = 'w';
-                if(st.st_mode & S_IXUSR) accMode[2] = 'x';
-                if(st.st_mode & S_IRGRP) accMode[3] = 'r';
-                if(st.st_mode & S_IWGRP) accMode[4] = 'w';
-                if(st.st_mode & S_IXGRP) accMode[5] = 'x';
-                if(st.st_mode & S_IROTH) accMode[6] = 'r';
-                if(st.st_mode & S_IWOTH) accMode[7] = 'w';
-                if(st.st_mode & S_IXOTH) accMode[8] = 'x';
+                for (int i = 0; i < 9; i++) accMode[i] = '-';
+                if (st.st_mode & S_IRUSR) accMode[0] = 'r';
+                if (st.st_mode & S_IWUSR) accMode[1] = 'w';
+                if (st.st_mode & S_IXUSR) accMode[2] = 'x';
+                if (st.st_mode & S_IRGRP) accMode[3] = 'r';
+                if (st.st_mode & S_IWGRP) accMode[4] = 'w';
+                if (st.st_mode & S_IXGRP) accMode[5] = 'x';
+                if (st.st_mode & S_IROTH) accMode[6] = 'r';
+                if (st.st_mode & S_IWOTH) accMode[7] = 'w';
+                if (st.st_mode & S_IXOTH) accMode[8] = 'x';
                 accMode[9] = '\0';
 
                 // wypisz na ekran :
                 printf("%s   %li   %s   %s\n", name, size, accMode, time);
             }
+        } else if (strcmp(file->d_name, ".") != 0 && strcmp(file->d_name, "..") != 0 && file->d_type == 4) {
+            // jesli katalog to :
+            char innerDirPath[256];
+            strcpy(innerDirPath, dirPath);
+            strcat(innerDirPath, file->d_name);
+            strcat(innerDirPath, "/");
+            printGreaterStat(innerDirPath,minSize);
         }
     }
+    closedir(dir);
 }
