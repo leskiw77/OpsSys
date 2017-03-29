78c7dec1c100405d58f16c2cf31fa228dbfe845e
Kamil Borowiec <kborowiec95@gmail.com>
2017-03-29 23:30:25 +0200

    cw04 zad1 skonczone

--- cw04/zad1/main.c
+++ cw04/zad1/main.c
@@ -3,28 +3,37 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <string.h>
 
-bool forward = true;
 
-void changeDirection() {
-    forward = !forward;
-}
+static bool forward = true;
+void changeDirection() { forward = !forward;  }
+
 
 // CTRL + C
 void reactSIGINT(int signal) {
-    printf("\n\nReceived SIGINT\n\n");
-    exit(1);
+    printf("\n\nReceived SIGINT -> end of the programme\n\n");
+    exit(0);
 }
 
 // CTRL + Z
 void reactSIGTSTP(int signal) {
     changeDirection();
-    printf("\n\nReceived SIGTSTP\n\n");
+    printf("\n\nReceived SIGTSTP -> changing direction\n\n");
 }
+
 
 int main(int argc, char **argv) {
     char letter = 'A';
     forward = true;
+
+    // ustawienie sigaction do oblugi SIGTSTP
+    struct sigaction act;
+    act.sa_handler = reactSIGTSTP;
+    sigemptyset(&act.sa_mask);
+    act.sa_flags = 0;
+    sigaction(SIGTSTP, &act, NULL);
+
     while(true) {
         printf("%c \n",letter);
         if(letter=='Z' && forward)       {  printf("\n"); letter = 'A'; }
@@ -32,14 +41,12 @@ int main(int argc, char **argv) {
         else if(forward)                 {  letter++; }
         else if(!forward)                {  letter--; }
 
+        // obluga SIGINT poprzed funkcje signal
         if(signal(SIGINT,reactSIGINT) == SIG_ERR) {
             printf("\n\nError while receiving signal SIGINT");
             exit(1);
         }
-        if(signal(SIGTSTP,reactSIGTSTP) == SIG_ERR) {
-            printf("\n\nError while receiving signal SIGSTP");
-            exit(1);
-        }
+
         usleep(250000);
     }
 }
