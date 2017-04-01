9301a568a0ccd6a68af17434465255fa0946b48a
Kamil Borowiec <kborowiec95@gmail.com>
2017-04-01 17:44:09 +0200

    child gotowy. 1 i 3 sposob tez

--- cw04/zad3/child.c
+++ cw04/zad3/child.c
@@ -3,47 +3,79 @@
 #include <unistd.h>
 #include <signal.h>
 
-//-------------------------------
 int counter = 0;
-void handler1(int);
-void handler2(int);
-//-------------------------------
 
-int main(int argc, char **argv) {
-    struct sigaction sa1, sa2;
-    sa1.sa_handler = handler1;
-    sa1.sa_flags = 0;
-    sigaddset(&(sa1.sa_mask),SIGRTMIN+1);
-    sa2.sa_handler = handler2;
-    sa2.sa_flags = 0;
-    sigaddset(&(sa2.sa_mask),SIGRTMIN);
-    if(sigaction(SIGRTMIN, &sa1, NULL) == -1 || sigaction(SIGRTMIN+1, &sa2, NULL) == -1) {
-        printf("Blad sigaction (child)\n");
-        exit(1);
-    }
-    sigset_t empty;
-    sigemptyset(&empty);
-    while(1)
-        sigsuspend(&empty);
-    return 0;
-}
-
-void handler1(int signum){
+void incrementCounter(int signum) {
     counter++;
 }
 
-void handler2(int signum){
+void resendSignals(int signum) {
     pid_t ppid = getppid();
-    while(counter) {
-        if((kill(ppid, SIGRTMIN)) != 0){
-            printf("Błąd funkcji kill (child)!\n");
+    for (int i = 0; i < counter; i++) {
+        if ((kill(ppid, SIGUSR1)) != 0) {
+            printf("\nChild : kill error\n");
             exit(1);
         }
-        counter--;
     }
-    if(kill(ppid, SIGRTMIN+1) != 0){
-        printf("Błąd funkcji kill (child)");
+    if (kill(ppid, SIGUSR2) != 0) {
+        printf("\nChild : kill error\n");
         exit(1);
     }
+    printf("\nChild : %d x SIGUSR1 received and resend parent\n", counter);
+    printf("Child : 1 x SIGUSR2 received and resend parent\n");
     exit(0);
+}
+
+void resendRTSignals(int signum) {
+    pid_t ppid = getppid();
+    for (int i = 0; i < counter; i++) {
+        if ((kill(ppid, SIGRTMIN)) != 0) {
+            printf("\nChild : kill error\n");
+            exit(1);
+        }
+    }
+    if (kill(ppid, SIGRTMIN+1) != 0) {
+        printf("\nChild : kill error\n");
+        exit(1);
+    }
+    printf("\nChild : %d x SIGRTMIN received and resend parent\n", counter);
+    printf("Child : 1 x SIGRTMIN+1 received and resend parent\n");
+    exit(0);
+}
+
+
+int main(int argc, char **argv) {
+    struct sigaction sAct1;
+    sAct1.sa_handler = incrementCounter;
+    sAct1.sa_flags = 0;
+    sigaddset(&(sAct1.sa_mask), SIGUSR2);
+
+    struct sigaction sAct2;
+    sAct2.sa_handler = resendSignals;
+    sAct2.sa_flags = 0;
+    sigaddset(&(sAct2.sa_mask), SIGUSR1);
+
+    struct sigaction sAct3;
+    sAct3.sa_handler = incrementCounter;
+    sAct3.sa_flags = 0;
+    sigaddset(&(sAct3.sa_mask), SIGRTMIN+1);
+
+    struct sigaction sAct4;
+    sAct4.sa_handler = resendRTSignals;
+    sAct4.sa_flags = 0;
+    sigaddset(&(sAct4.sa_mask), SIGRTMIN);
+
+    // ustawienie sigactions :
+    if ( sigaction(SIGUSR1, &sAct1, NULL) == -1 || sigaction(SIGUSR2, &sAct2, NULL) == -1 ||
+         sigaction(SIGRTMIN, &sAct3, NULL) == -1 || sigaction(SIGRTMIN+1, &sAct4, NULL) == -1) {
+        printf("\nChild : sigaction error\n");
+        exit(1);
+    }
+
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    while (1) {
+        sigsuspend(&sigset);
+    }
+    return 0;
 }
--- cw04/zad3/parent.c
+++ cw04/zad3/parent.c
@@ -1,80 +1,147 @@
 #include <stdio.h>
-#include <stdlib.h> //atoi
-#include <unistd.h> //fork
+#include <stdlib.h>
+#include <unistd.h>
 #include <signal.h>
 
-//-------------------------------
 int counter = 0;
-int N;
-void handler1(int);
-void handler2(int);
-//-------------------------------
+int L;
 
-int main(int argc, char **argv) {
-    int i;
-    pid_t child;
-
-    if(argc != 2){
-        printf("Niepoprawna ilość parametrów wywołania!\n");
-        exit(1);
-    }
-    N = atoi(argv[1]);
-
-    struct sigaction sa1, sa2;
-    sa1.sa_handler = handler1;
-    sa1.sa_flags = 0;
-    sigaddset(&(sa1.sa_mask),SIGRTMIN+1);
-    //podczas obsługi sygnału SIGRTMIN
-    //ignorujemy sygnał SIGRTMIN+1
-
-    sa2.sa_handler = handler2;
-    sa2.sa_flags = 0;
-    sigaddset(&(sa2.sa_mask),SIGRTMIN);
-    //analogicznie
-
-    if(sigaction(SIGRTMIN, &sa1, NULL) == -1 || sigaction(SIGRTMIN+1, &sa2, NULL) == -1) {
-        printf("Blad sigaction (parent)\n");
-        exit(1);
-    }
-
-    sigset_t new, old;
-    sigaddset(&new, SIGRTMIN);
-    sigaddset(&new, SIGRTMIN+1);
-    sigprocmask(SIG_BLOCK, &new, &old);
-
-    child = fork();
-    if (child == -1) {
-        printf("Błąd funkcji fork! (parent)\n");
-        exit(1);
-    } else if(child == 0) {
-        if(execl("./child", "child", NULL) == -1){
-            printf("Błąd funkcji execl!\n");
-            exit(1);
-        }
-    }
-
-    for(i = 0; i < N; i++) {
-        if((kill(child, SIGRTMIN)) != 0) {
-            printf("Błąd funkcji kill (parent)\n");
-            exit(1);
-        }
-    }
-    if((kill(child, SIGRTMIN+1)) != 0) {
-        printf("Błąd funkcji kill (parent)\n");
-        exit(1);
-    }
-    while(1) sigsuspend(&old);
-
-    return 0;
-}
-
-void handler1(int signum){
+void incrementCounter(int signum) {
     counter++;
 }
 
-void handler2(int signum){
-    printf("\n%d sygnałów SIGUSR1 zostało wysłanych do child\n"
-                   "%d sygnałów SIGUSR1 zostało odebranych przez parent\n",
-           N, counter);
+void reactSIGUSR2(int signum) {
+    printf("\nParent : %d x SIGUSR1 send to child\n",L);
+    printf("Parent :  1 x SIGUSR2 send to child\n");
+    printf("\nParent : %d x SIGUSR1 received from child\n",counter);
+    printf("Parent :  1 x SIGUSR2 received from child\n");
     exit(0);
+}
+
+void reactRT2(int signum) {
+    printf("\nParent : %d x SIGRTMIN send to child\n",L);
+    printf("\nParent : %d x SIGRTMIN received from child\n",counter);
+    exit(0);
+}
+
+void sendNSignalsWithKillAndWait(int N, int signal1, int signal2) {
+    pid_t child;
+
+    // ? :
+    sigset_t newSet, oldSet;
+    sigaddset(&newSet, signal1);
+    sigaddset(&newSet, signal2);
+    sigprocmask(SIG_BLOCK, &newSet, &oldSet);
+
+    // fork :
+    child = fork();
+    if (child == 0) {
+        if (execl("./child", "child", NULL) == -1) {
+            printf("Parent : execl error\n");
+            exit(1);
+        }
+    } else if (child == -1) {
+        printf("Parent : fork error\n");
+        exit(1);
+    }
+
+    //wyslanie L sygnalow SIGNAL1 do child'a :
+    for (int i = 0; i < N; i++) {
+        if ((kill(child, signal1)) != 0) {
+            printf("\nParent : kill error\n");
+            exit(1);
+        }
+    }
+
+    // wyslanie signal2 do child'a :
+    if ((kill(child, signal2)) != 0) {
+        printf("\nParent : kill error\n");
+        exit(1);
+    }
+
+    while (1) {
+        sigsuspend(&oldSet);
+    }
+}
+
+int main(int argc, char **argv) {
+    pid_t child;
+
+    if (argc != 3) {
+        printf("Wrong arguments\n");
+        exit(1);
+    }
+    L = atoi(argv[1]);
+    int type = atoi(argv[2]);
+
+    struct sigaction sAct1;
+    sAct1.sa_flags = 0;
+    struct sigaction sAct2;
+    sAct2.sa_flags = 0;
+
+    if(type==1) {
+        // typ1 : SIGUSR1, SIGUSR2 za pomocą funkcji kill
+
+        // ustawienie sigaction :
+        sAct1.sa_handler = incrementCounter;
+        sigaddset(&(sAct1.sa_mask), SIGUSR1);
+
+        sAct2.sa_handler = reactSIGUSR2;
+        sigaddset(&(sAct2.sa_mask), SIGUSR2);
+
+        if (sigaction(SIGUSR1, &sAct1, NULL) == -1 || sigaction(SIGUSR2, &sAct2, NULL) == -1) {
+            printf("Parent : sigaction error\n");
+            exit(1);
+        }
+        sendNSignalsWithKillAndWait(L,SIGUSR1,SIGUSR2);
+    } else if(type==2) {
+        // typ2 : SIGUSR1, SIGUSR2 za pomocą funkcji sigqueue
+
+    } else if(type==3) {
+        // typ3 : wybrane 2 sygnały czasu rzeczywistego za pomocą kill
+
+        sAct1.sa_handler = incrementCounter;
+        sigaddset(&(sAct1.sa_mask),SIGRTMIN+1);
+
+        sAct2.sa_handler = reactRT2;
+        sigaddset(&(sAct2.sa_mask),SIGRTMIN);
+
+        if(sigaction(SIGRTMIN, &sAct1, NULL) == -1 || sigaction(SIGRTMIN+1, &sAct2, NULL) == -1) {
+            printf("Blad sigaction (parent)\n");
+            exit(1);
+        }
+
+        sigset_t newSet, oldSet;
+        sigaddset(&newSet, SIGRTMIN); sigaddset(&newSet, SIGRTMIN+1);
+        sigprocmask(SIG_BLOCK, &newSet, &oldSet);
+
+        child = fork();
+        if (child == -1) {
+            printf("Błąd funkcji fork! (parent)\n");
+            exit(1);
+        } else if(child == 0) {
+            if(execl("./child", "child", NULL) == -1){
+                printf("Błąd funkcji execl!\n");
+                exit(1);
+            }
+        }
+
+        for(int i = 0; i < L; i++) {
+            if((kill(child, SIGRTMIN)) != 0) {
+                printf("Błąd funkcji kill (parent)\n");
+                exit(1);
+            }
+        }
+        if((kill(child, SIGRTMIN+1)) != 0) {
+            printf("Błąd funkcji kill (parent)\n");
+            exit(1);
+        }
+        
+        while(1) sigsuspend(&oldSet);
+
+    } else {
+        printf("Wrong arguments\n");
+        exit(1);
+    }
+    return 0;
 }
