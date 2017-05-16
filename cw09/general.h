7b93d0afab5ab78181a32207f26dbfb18f15ad1b
jarema <jarema.leskiw@gmail.com>
2017-05-16 16:52:27 +0200

    without synchronization

---
+++ cw09/general.h
@@ -0,0 +1,18 @@
+#ifndef SYS_GENERAL_H
+#define SYS_GENERAL_H
+
+#define ARRAYSIZE 10
+#define MAXOPERATION 10
+#define MAXNUMBER 10
+#define MAXDEVIDER 5
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+
+#endif
